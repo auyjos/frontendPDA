2bab69403e146a6d187641532c04c7532ffd0296
auyjos <josea13p@gmail.com>
2023-11-16 19:01:35 -0600

    Agregar más información del usuario

--- src/views/UsuarioView.vue
+++ src/views/UsuarioView.vue
@@ -22,7 +22,7 @@ console.log(fullName);
         />
       </div>
       <div class="w-1/4">
-        <h2 class="text-2xl font-bold">Nombre: {{ fullName }}</h2>
+        <h2 class="text-2xl font-bold">Full name: {{ fullName }}</h2>
       </div>
       <div class="w-1/2 text-right">
         <RouterLink
@@ -35,20 +35,13 @@ console.log(fullName);
     </div>
     <div class="flex flex-row">
       <h2 class="text-2xl font-bold mb-4">
-        Datos Usuario
+        ID Usuario:
         {{ store.selectedUser?.id }}
       </h2>
     </div>
     <div>
       <div class="bg-white overflow-hidden shadow rounded-lg border">
-        <div class="px-4 py-5 sm:px-6">
-          <h3 class="text-lg leading-6 font-medium text-gray-900">
-            User Profile
-          </h3>
-          <p class="mt-1 max-w-2xl text-sm text-gray-500">
-            This is some information about the user.
-          </p>
-        </div>
+        <div class="px-4 py-5 sm:px-6"></div>
         <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
           <dl class="sm:divide-y sm:divide-gray-200">
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -58,22 +51,27 @@ console.log(fullName);
               </dd>
             </div>
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Gender</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {{ store.selectedUser?.gender }}
+              </dd>
+            </div>
+            <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Email address</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                johndoe@example.com
+                {{ store.selectedUser?.email }}
               </dd>
             </div>
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Phone number</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                (123) 456-7890
+                {{ store.selectedUser?.phone }}
               </dd>
             </div>
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium text-gray-500">Address</dt>
               <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                123 Main St<br />
-                Anytown, USA 12345
+                {{ store.selectedUser?.location }}
               </dd>
             </div>
           </dl>
